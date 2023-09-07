c1fedadbce6f1e41946786141f7204a589cc357d
YassineQaanabou <qaanabou@gmail.com>
2023-09-07 10:52:48 +0200

    Milestone 3 completed

--- src/main/java/org/nations/Nations.java
+++ src/main/java/org/nations/Nations.java
@@ -1,22 +1,26 @@
 package org.nations;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Nations {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         String url = "jdbc:mysql://localhost:3306/db_nations";
         String user = "root";
         String password = "Root6912";
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            System.out.println(conn.getCatalog());
+            System.out.println("Che nazione vuole cercare?");
+            String search = scan.nextLine();
 
-            String sql = """
-                    select c.name as nation , c.country_id , r.name as region ,c2.name as continent
-                    from countries c\s
-                    join regions r  on r.region_id =c.region_id\s
-                    join continents c2 on c2.continent_id =r.continent_id
-                    order by c.name asc ;""";
+
+            String sql = "select c.name as nation , c.country_id , r.name as region ,c2.name as continent\n" +
+                    "from countries c \n" +
+                    "join regions r  on r.region_id =c.region_id \n" +
+                    "join continents c2 on c2.continent_id =r.continent_id\n" +
+                    "where c.name like '%" + search + "%'" +
+                    "order by c.name asc ;";
             try (PreparedStatement ps = conn.prepareStatement(sql)) {
                 try (ResultSet rs = ps.executeQuery()) {
                     while (rs.next()) {
